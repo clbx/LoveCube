b3c8b5689735a2ba4183ab583c6c654c0bfc564d
Clay Buxton <clay@clbx.io>
2020-07-15 23:54:27 -0400

    lotta work

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# LoveCube
-A WiFi Connected Message Box using the ESP32
+## LoveCube
+
+A Wifi Enabled Message Box based on the ESP32
+
+-----
+
+LoveCube is an Open Source software and hardware version of the [LoveBox](https://en.lovebox.love).
+
+All wirinng diagrams, code, and 3D models are/will be available in this repository. The Love Box is $100, which I thought was way too much for a chip and a box. The love cube can be made for under $30, had the potential to have a lot more functionality, and doesn't have the ugly ass heart on the front of it to notify a new message (this has been replaced with slick underglow LEDs).
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-
 #include <WiFi.h>
 #include <Wire.h>
 #include <Arduino.h>
-#include "ESPAsyncWebServer.h"
+#include <ESPAsyncWebServer.h>
+#include <SPI.h>
+#include <SSD_13XX.h>
 #include "credentials.h"
 
 Credentials creds;
@@ -11,9 +12,20 @@ const char* ssid = creds.ssid;
 const char* password = creds.pass;
 bool newmsg = false;
 
+
+
 #define DAC1 25
 #define DAC2 26
 #define BUTTON 35
+
+#define _cs   17
+#define _dc   16
+#define _mosi 23
+#define _sclk 18
+#define _rst  5
+
+SSD_13XX tft = SSD_13XX(_cs, _dc);
+uint8_t errorCode = 0;
 
 int left = 120; //255= 3.3V 128=1.65V
 int right = 180;
@@ -27,8 +39,24 @@ void setup() {
   pinMode(BUTTON,INPUT);
   Serial.begin(9600);
 
-  //lcd.init();
-  //lcd.backlight();
+  long unsigned debug_start = millis();
+  while (!Serial && ((millis() - debug_start) <= 5000));
+  tft.begin(false);
+  delay(30);
+  //the following it's mainly for Teensy
+  //it will help you to understand if you have choosed the
+  //wrong combination of pins!
+  errorCode = tft.getErrorCode();
+  if (errorCode != 0) {
+	  Serial.print("Init error! ");
+	  if (bitRead(errorCode, 0)) Serial.print("MOSI or SCLK pin mismach!\n");
+	  if (bitRead(errorCode, 1)) Serial.print("CS or DC pin mismach!\n");
+  }
+  else {
+	  Serial.println(F("Benchmark Sketch V1.1"));
+	  tft.setTextColor(WHITE);
+	  tft.setTextScale(2);
+  }
  
   WiFi.begin(ssid, password);
  
@@ -36,6 +64,7 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
+  
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
  
@@ -45,18 +74,14 @@ void setup() {
     [](AsyncWebServerRequest * request){},
     NULL,
     [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
- 
+      tft.clearScreen();
       for (size_t i = 0; i < len; i++) {
         newMsg = true;
-        Serial.write(data[i]);
-        
+        tft.print((char)data[i]);
       }
- 
       Serial.println();
- 
       request->send(200);
   });
- 
   server.begin();
 }
  
